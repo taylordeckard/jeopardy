c7ddb9f5a6e2c9c188633619ca5d2ecfd9af022f
taylordeckard <taylor999@gmail.com>
2018-08-05 22:42:43 -0400

    added vue frontend

---
+++ client/src/src/components/MainWindow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main"></div>
+</template>
+
+<script>
+export default {
+  name: 'MainWindow',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main {
+  background: #071484;
+  height: 100%;
+  width: 100%;
+}
+</style>
